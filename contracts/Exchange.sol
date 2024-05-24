b73578c987547964deac2ac2c863b1bb289a1073
sgkrc <martinkim99@gmail.com>
2024-05-24 22:13:09 +0900

    Liquidity

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -4,25 +4,75 @@ pragma solidity ^0.8.9;
 import "hardhat/console.sol";
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract Exchange {
-    IERC20 token;
+contract Exchange is ERC20 {
+    //token에 public을 지정해주니 compile error 해결
+    IERC20 public token;
 
-    constructor (address _token) {
+    constructor (address _token) ERC20("Gray Uniswap V2", "GUNI-V2") {
         token = IERC20(_token);
+        //값을 상수로 고정하는 이유는 악의적인 사용자가 만든 유사한 LP 토큰 이더의 판매 등을 막기 위해
+        //무슨 토큰의 페어인지 이름만으로는 알 수 없도록 고정시켜놓은 것
     }
     
-    function addLiquidity(uint256 _tokenAmount) public payable {
-        token.transferFrom(msg.sender, address(this), _tokenAmount);
+    //유동성 공급
+    function addLiquidity(uint256 _maxTokens) public payable {
+        uint256 totalLiquidity = totalSupply();
+        if (totalLiquidity > 0){
+            uint256 ethReserve = address(this).balance - msg.value;
+            uint256 tokenReserve = token.balanceOf(address(this));
+            uint256 tokenAmount = msg.value * tokenReserve / ethReserve;
+            require(_maxTokens >= tokenAmount);
+            token.transferFrom(msg.sender, address(this), tokenAmount);
+            uint256 liquidityMinted = totalLiquidity * msg.value / ethReserve;
+            _mint(msg.sender, liquidityMinted);
+        }else{
+            //유동성이 없는 상황에서 유동성 공급
+            uint256 tokenAmount = _maxTokens;
+            uint256 initialLiquidity = address(this).balance;
+            _mint(msg.sender, initialLiquidity); //유동성 공급을 한 msg.sender에게 그만큼의 LP 토큰 발급
+            token.transferFrom(msg.sender, address(this), tokenAmount);
+        }
+    }
+
+    //유동성 제거
+    function removeLiquidity(uint256 _lpTokenAmount) public {
+        uint256 totalLiquidity = totalSupply();
+        uint256 ethAmount = _lpTokenAmount * address(this).balance / totalLiquidity;
+        //내가 받게될 토큰의 양
+        uint256 tokenAmount = _lpTokenAmount * token.balanceOf(address(this)) / totalLiquidity;
+
+        _burn(msg.sender, _lpTokenAmount);
+
+        //위에서 계산한만큼의 eth와 토큰을 나에게 보내라
+        payable(msg.sender).transfer(ethAmount);
+        token.transfer(msg.sender, tokenAmount);
     }
   
     // ETH -> ERC20
-    function ethToTokenSwap() public payable {
-        uint256 inputAmount = msg.value;
+    function ethToTokenSwap(uint256 _minTokens) public payable {
         // calculate amount out (zero fee)
-        uint256 outputAmount = inputAmount;
+        // msg.value만큼 빠지는 이유는 이미 컨트랙트에서 해당 함수를 실행하면 이더리움이 넘어온 상태인데
+        //address(this).balance를 찍어보면 내가 스왑할 때 입력한 이더리움 개수가 추가되어 있기 때문에
+        //내가 이더를 보낸 값만큼 빼야 기존에 있던 풀의 이더리움 양을 얻어올 수 있음
+        uint256 outputAmount = getOutputAmount(msg.value, address(this).balance - msg.value, token.balanceOf(address(this)));
+        require(outputAmount >= _minTokens, "Inffucient outputamount");
         //transfer token out
-        IERC20(token).transfer(msg.sender, outputAmount);
+        //컨트랙트가 보유중인 토큰을 msg.sender인 사용자에게 전송
+        token.transfer(msg.sender, outputAmount);
+    }
+
+    // ERC20 -> ETH
+    function tokenToEthSwap(uint256 _tokenSold, uint256 _minEth) public payable {
+        // calculate amount out (zero fee)
+        uint256 outputAmount = getOutputAmount(_tokenSold, token.balanceOf(address(this)), address(this).balance);
+        require(outputAmount >= _minEth, "Inffucient outputamount");
+        
+        //transfer token out
+        //해당 주소로 _tokenSold만큼 전송
+        token.transferFrom(msg.sender, address(this), _tokenSold);
+        payable(msg.sender).transfer(outputAmount);
     }
 
     function getPrice(uint256 inputReserve, uint256 outputReserve) public pure returns (uint256) {
@@ -31,5 +81,11 @@ contract Exchange {
         return numerator / denominator;
     }
 
+    // CPMM 알고리즘
+    function getOutputAmount(uint256 inputAmount, uint256 inputReserve, uint256 outputReserve) public pure returns (uint256) {
+        uint256 numerator = outputReserve * inputAmount;
+        uint256 denominator = inputReserve + inputAmount;
+        return numerator / denominator;
+    }
 }
 
--- test/Exchange.ts
+++ test/Exchange.ts
@@ -30,10 +30,47 @@ describe("Exchange", () => {
 
     describe("addLiquidity", async () => {
         it("add liquidity", async () => {
-          await token.approve(exchange.address, toWei(1000));
-          await exchange.addLiquidity(toWei(1000), { value: toWei(1000) });
+          await token.approve(exchange.address, toWei(500));
+          await exchange.addLiquidity(toWei(500), { value: toWei(1000) });
           expect(await getBalance(exchange.address)).to.equal(toWei(1000));
-          expect(await token.balanceOf(exchange.address)).to.equal(toWei(1000));
+          expect(await token.balanceOf(exchange.address)).to.equal(toWei(500));
+
+          await token.approve(exchange.address, toWei(100));
+          await exchange.addLiquidity(toWei(100), { value: toWei(200) });
+          expect(await getBalance(exchange.address)).to.equal(toWei(1200));
+          expect(await token.balanceOf(exchange.address)).to.equal(toWei(600));
+        });
+      });
+
+
+    describe("removeLiquidity", async () => {
+        it("remove liquidity", async () => {
+          await token.approve(exchange.address, toWei(500));
+          await exchange.addLiquidity(toWei(500), { value: toWei(1000) });
+          expect(await getBalance(exchange.address)).to.equal(toWei(1000));
+          expect(await token.balanceOf(exchange.address)).to.equal(toWei(500));
+
+          await token.approve(exchange.address, toWei(100));
+          await exchange.addLiquidity(toWei(100), { value: toWei(200)});
+          expect(await getBalance(exchange.address)).to.equal(toWei(1200));
+          expect(await token.balanceOf(exchange.address)).to.equal(toWei(600));
+          
+          //여기까지 풀에 있는 eth가 1200, gray가 600이므로
+          //600개의 유동성을 제거하면 1200개 중 절반이므로 각 풀에 남아있는 양도 절반이 된다.
+          await exchange.removeLiquidity(toWei(600));
+          expect(await getBalance(exchange.address)).to.equal(toWei(600));
+          expect(await token.balanceOf(exchange.address)).to.equal(toWei(300));
+        });
+      });
+
+    describe("getOutputAmount", async () => {
+        it("correct getOutputAmount", async () => {
+          await token.approve(exchange.address, toWei(4000));
+          //4:1
+          await exchange.addLiquidity(toWei(4000), { value: toWei(1000) });
+          
+          //get 1ETH : ??GRAY
+          console.log(toEther(await exchange.getOutputAmount(toWei(1), getBalance(exchange.address), token.balanceOf(exchange.address))));
         });
       });
 
@@ -53,25 +90,28 @@ describe("Exchange", () => {
         })
     })
 
-    describe("EthToTokenSwap", async() => {
-        it("correct EthToTokenSwap", async() => {
+    describe("ethToTokenSwap", async() => {
+        it("correct ethToTokenSwap", async() => {
+            await token.approve(exchange.address, toWei(4000));
+            
+            // GRAY : ETH 4:1
+            await exchange.addLiquidity(toWei(4000), { value: toWei(1000) });
+            
+            // 1ETH : ?? GRAY
+            await exchange.connect(user).ethToTokenSwap(toWei(3.99), {value: toWei(1)});
 
-            await token.approve(exchange.address, toWei(1000));
-            await exchange.addLiquidity(toWei(1000), { value: toWei(1000) });
-      
-            await exchange.connect(user).ethToTokenSwap({ value: toWei(1) });
+            console.log(toEther(await token.balanceOf(user.address)));
+            // expect(
+            //     (toEther(await token.balanceOf(user.address)))
+            // ).to.eq("1.0");
 
-            expect(
-                (toEther(await token.balanceOf(user.address)))
-            ).to.eq("1.0");
+            // expect(
+            //     (toEther(await token.balanceOf(exchange.address)))
+            // ).to.eq("999.0");
 
-            expect(
-                (toEther(await token.balanceOf(exchange.address)))
-            ).to.eq("999.0");
-
-            expect(
-                (toEther(await getBalance(exchange.address)))
-            ).to.eq("1001.0");
+            // expect(
+            //     (toEther(await getBalance(exchange.address)))
+            // ).to.eq("1001.0");
             
         })
     })
